c75f305f119b483c54a35c00fafaa434d1936079
srijay-095 <srijaybanda095@gmail.com>
2026-07-06 19:45:15 +0530

    Time: 0 ms (100%), Space: 28.1 MB (66.88%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -21,14 +21,7 @@ public:
             }
         }
         int k=0;
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]==el)
-            {
-                k++;
-            }
-        }
-        if(k>n/2) return el;
-       return 0 ;
+         return el;
+       
     }
 };
