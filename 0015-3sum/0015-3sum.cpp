b670c2a36f38e6461e6dc807e811c0c7c71caa54
srijay-095 <srijaybanda095@gmail.com>
2026-07-09 11:42:17 +0530

    Time: 51 ms (44.09%), Space: 30.9 MB (25.39%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -6,7 +6,7 @@ public:
         sort(nums.begin(),nums.end());
         for(int i=0;i<n;i++)
         {
-            if(i>0 && nums[i]==nums[i-1]) continue;
+            if(i>0 && nums[i-1]==nums[i]) continue;
             int j=i+1;
             int k=n-1;
             while(j<k)
@@ -22,17 +22,17 @@ public:
                 }
                 else
                 {
-                    vector<int> t={nums[i],nums[j],nums[k]};
-                    ans.push_back(t);
+                    vector<int> temp={nums[i],nums[j],nums[k]};
+                    ans.push_back(temp);
                     j++;
                     k--;
                     while(j<k && nums[j]==nums[j-1]) j++;
                     while(j<k && nums[k]==nums[k+1]) k--;
+
                 }
             }
         }
         return ans;
-
         
     }
 };
